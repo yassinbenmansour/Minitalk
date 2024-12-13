8f294b1a382eebc6790daf0c113a0c12648bdaf0
yabenman <benmasour.yassine@gmail.com>
2024-12-13 06:11:26 +0100

    -_- in progressssssss

---
+++ minitalk.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yabenman <yabenman@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/13 06:07:30 by yabenman          #+#    #+#             */
+/*   Updated: 2024/12/13 06:08:14 by yabenman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+void	ft_putstr(char *str);
+int		ft_atoi(const char *str);
+void	ft_putnbr(int n);
+void	ft_putchar(char c);
+
+#endif
---
+++ minitalk_utils.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_utils.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yabenman <yabenman@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/13 06:08:49 by yabenman          #+#    #+#             */
+/*   Updated: 2024/12/13 06:09:04 by yabenman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+int	ft_atoi(const char *str)
+{
+	int	result;
+	int	sign;
+	int	i;
+
+	i = 0;
+	result = 0;
+	sign = 1;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return (result * sign);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int n)
+{
+	long	x;
+
+	x = n;
+	if (x < 0)
+	{
+		x = -x;
+		write(1, "-", 1);
+	}
+	if (x <= 9)
+	{
+		x = x + 48;
+		write(1, &x, 1);
+	}
+	else
+	{
+		ft_putnbr(x / 10);
+		ft_putnbr(x % 10);
+	}
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return ;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
